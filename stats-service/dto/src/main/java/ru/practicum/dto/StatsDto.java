dfbcb2ecc2c8a6c7e10d9b724535d5c52bb75c08
asdqwe <asdaedadasczx@mail.ru>
2024-03-13 01:15:20 +0200

    java-explore-with-me

---
+++ stats-service/dto/src/main/java/ru/practicum/dto/StatsDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class StatsDto {
+    String app;
+    String uri;
+    Long hits;
+
+    public StatsDto(String app, String uri, Long hits) {
+        this.app = app;
+        this.uri = uri;
+        this.hits = hits;
+    }
+}
---
+++ stats-service/server/src/main/java/ru/practicum/Hit.java
@@ -0,0 +1,28 @@
+package ru.practicum;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hits")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Hit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column
+    String app;
+    @Column
+    String uri;
+    @Column
+    String ip;
+    @Column(name = "time_stamp")
+    LocalDateTime timestamp;
+}
